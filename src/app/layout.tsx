ce4add8417a5906a6098b2b7556db756c723b37e
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2026-03-04 18:13:33 +0100

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ import StyledComponentsRegistry from "@/lib/registry";
 export const metadata: Metadata = {
   title: "Country Explorer",
   description: "Explore countries, borders, regions and details",
+  icons: {
+    icon: [
+      { url: "/icon192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon512.png", sizes: "512x512", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
